e3a9da1ce3ee6dae7885331729c4cecc60160198
Matt Snyder <matt@cranialpulse.com>
2021-05-19 14:34:32 -0400

    Apply ten off discount, noted design debt

--- app/src/main/java/henrys/TenOffApples.java
+++ app/src/main/java/henrys/TenOffApples.java
@@ -4,9 +4,12 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 
 public class TenOffApples implements DiscountStrategy {
+  static final BigDecimal DISCOUNT = new BigDecimal(".01"); 
+
   @Override
   public BigDecimal getDiscount(Basket basket){
-    return BigDecimal.ZERO;
+    BigDecimal num_apples = new BigDecimal(basket.contains("apples"));
+    return DISCOUNT.multiply(num_apples); // DEBT: Should pull price from either basket of master price list
   }
 
   @Override
--- app/src/test/java/henrys/TenOffApplesTest.java
+++ app/src/test/java/henrys/TenOffApplesTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class TenOffApplesTest {
   @Test public void testOnEmptyBasket() {
     assertFalse(new TenOffApples().isApplicable(new Basket()));
-    assertEquals(BigDecimal.ZERO, new TenOffApples().getDiscount(new Basket()));
+    assertEquals(new BigDecimal("0.00"), new TenOffApples().getDiscount(new Basket()));
   }  
 
   @Test public void testWhenOneUnitOfApples() {
@@ -16,6 +16,6 @@ public class TenOffApplesTest {
     basketWithApples.addItem(new StockItemBuilder().withName("apples").withPrice("0.10").build());
 
     assertTrue(new TenOffApples().isApplicable(basketWithApples));
-    // assertEquals(new BigDecimal("0.01"), new TenOffApples().getDiscount(basketWithApples));
+    assertEquals(new BigDecimal("0.01"), new TenOffApples().getDiscount(basketWithApples));
   }
 }
