e1b28fa3f0a0cd54bdd48c2d14426fb1074e6817
Matt Snyder <matt@cranialpulse.com>
2021-05-19 11:22:40 -0400

    Calculate basket total

--- app/src/main/java/henrys/Basket.java
+++ app/src/main/java/henrys/Basket.java
@@ -1,9 +1,22 @@
 package henrys;
 
+import henrys.StockItem;
+import java.util.ArrayList;
 import java.math.BigDecimal;
 
 public class Basket {
+  private final ArrayList<StockItem> contents = new ArrayList<StockItem>();
+
   public BigDecimal getTotal(){
-    return new BigDecimal(0);
+    BigDecimal total = contents
+      .stream()
+      .map(i -> i.getPrice())
+      .reduce(BigDecimal.ZERO, BigDecimal::add);
+    return total;
+  }
+
+  public Basket addItem(StockItem itemToAdd){
+    contents.add(itemToAdd);
+    return this;
   }
 }
--- app/src/test/java/henrys/BasketTest.java
+++ app/src/test/java/henrys/BasketTest.java
@@ -11,4 +11,13 @@ public class BasketTest {
     Basket emptyBasket = new Basket();
     assertEquals(new BigDecimal(0), emptyBasket.getTotal());
   }
+
+  @Test public void testBasketWithAUnitOfApplesTotalIsAccurate(){
+    Basket basket = new Basket();
+    StockItem apples = new StockItem("apples", "single", new BigDecimal(0.10));
+
+    basket.addItem(apples);
+
+    assertEquals(new BigDecimal(0.10), basket.getTotal());
+  }
 }
